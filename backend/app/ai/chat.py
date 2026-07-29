b10e255a4067b9bb54e987e0e3566e06a9118084
Anup Raj <anup.202104091@tulas.edu.in>
2026-07-29 23:14:52 +0530

    debugged error in chat.py

--- backend/app/ai/chat.py
+++ backend/app/ai/chat.py
@@ -4,6 +4,6 @@ from app.core.config import settings
 
 llm = ChatMistralAI(
     model="mistral-small-2506",
-    google_api_key=settings.MISTRAL_API_KEY,
+    mistral_api_key=settings.MISTRAL_API_KEY,
     temperature=0,
 )
